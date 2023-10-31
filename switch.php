34d092ba4e43f7790f319099fb84838ed2e04579
NSIX06 <felipebugalho2016@gmail.com>
2023-10-31 09:47:27 -0300

    final

---
+++ switch.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="">
+    <label >Informe a cor</label>
+        <input type="text" name="color">
+        <input type="submit" value="Verificar">
+    </form>
+
+    
+<?php 
+
+
+
+
+
+      if  (isset($_GET["color"]))
+      {
+        $color = $_GET["color"];
+        switch($color) 
+        { 
+        case "vermelho":
+            echo "Olá!";
+            break;
+        case  "verde":
+            echo "Bem-vindo!";
+            break;
+        }
+    }
+
+
+
+
+
+?>
+
+</body>
+</html>
